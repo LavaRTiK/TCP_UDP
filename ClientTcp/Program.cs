8bf291c9f33c9d441b36dfb269bfb3d6117dfb08
Паша Ярошенко <pavlo.legominecraft@gmail.com>
2022-07-16 23:30:04 +0300

    Добавьте файлы проекта.

---
+++ ClientTcp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientTcp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var tcpSoket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            Console.WriteLine("Введите собщения");
+            var massage = Console.ReadLine();
+
+            var data = Encoding.UTF8.GetBytes(massage);
+
+            tcpSoket.Connect(tcpEndPoint);
+
+            tcpSoket.Send(data);
+
+            var buffer = new byte[256];
+            var size = 0;
+            var answer = new StringBuilder();
+
+            do
+            {
+                size = tcpSoket.Receive(buffer);
+                answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+            }
+            while (tcpSoket.Available > 0);
+
+            Console.WriteLine(answer.ToString());
+            tcpSoket.Shutdown(SocketShutdown.Both);
+
+            tcpSoket.Close();
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ TCP_UDP/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_UDP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip),port);
+            
+            var tcpSoket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            tcpSoket.Bind(tcpEndPoint);
+            tcpSoket.Listen(5);
+
+            while (true)
+            {
+                var listener = tcpSoket.Accept();
+                var buffer = new byte[256];
+                var size = 0;
+                var data = new StringBuilder();
+                do
+                {
+                    size = listener.Receive(buffer);
+                    data.Append(Encoding.UTF8.GetString(buffer,0,size));
+                }
+                while (listener.Available > 0);
+                Console.WriteLine(data);
+
+                listener.Send(Encoding.UTF8.GetBytes("Успех"));
+                listener.Shutdown(SocketShutdown.Both);
+                listener.Close();
+            }
+        }
+    }
+}
